4cfa0fd30ccce6682b9c26539702a4a6a658a54b
Ali <shabbirali54hassan@gmail.com>
2024-04-04 10:50:59 -0700

    create-commponents

--- src/app/Components/Dashboard/dashboard/dashboard.component.ts
+++ src/app/Components/Dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,SidebarComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+dashboardList:any[]=[
+{
+  name:'Dashboard',icon:'bi bi-grid-fill'
+},
+{
+  name:'Products',icon:'bi bi-grid-fill'
+},
+{
+  name:'Inventory',icon:'bi bi-grid-fill'
+},
+{
+  name:'Orders',icon:'bi bi-grid-fill'
+},
+{
+  name:'Sales',icon:'bi bi-grid-fill'
+},
+{
+  name:'Customers',icon:'bi bi-grid-fill'
+}
+,
+{
+  name:'Logout',icon:'bi bi-grid-fill'
+}
+];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { DashboardComponent } from './Components/Dashboard/dashboard/dashboard.component';
+import { HomeComponent } from './Components/Dashboard/dashboard/home/home.component';
+import { ProductsComponent } from './Components/Dashboard/dashboard/products/products.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'login',pathMatch:'full'},
     {path:'login',component:LoginComponent},
-    {path:'dashboard',component:DashboardComponent}
+    {
+        path:'dashboard',component:DashboardComponent,
+        children:[
+           {path:'',component:HomeComponent},
+           {path:'product',component:ProductsComponent}
+
+        ]
+    }
 
 ];
